4534e9b394107137e98a05ab1e9e968ec3509168
jponader <jponader@knights.ucf.edu>
2020-05-07 11:16:02 -0400

    Updated Testing Functions

--- Testing/VGG/network.py
+++ Testing/VGG/network.py
@@ -7,7 +7,7 @@ from MILR.MILR import MILR
 import tensorflow as tf
 from tensorflow import keras
 from keras import optimizers
-from keras.applications.vgg19 import VGG19, preprocess_input, decode_predictions
+from keras.applications.vgg16 import VGG16, preprocess_input, decode_predictions
 from keras.preprocessing.image import load_img, img_to_array
 from keras.utils import to_categorical, HDF5Matrix
 
@@ -21,7 +21,7 @@ import pathlib
 import random
 import h5py
 
-model = VGG19(weights='imagenet')
+model = VGG16(weights='imagenet')
 
 #keras.backend.set_learning_phase(0)
 
@@ -44,6 +44,7 @@ all_image_paths = list(data_root.glob('*'))
 all_image_paths = [str(path) for path in all_image_paths]
 all_image_paths.sort()
 
+testSize = 10000
 
 imgs = []
 i = 1
@@ -55,6 +56,10 @@ for paths in all_image_paths:
 	input_image = np.expand_dims(numpy_image, axis=0) 
 	input_image = preprocess_input(input_image)
 	imgs.append(input_image[0])
+	if i == testSize:
+		break
+
+	i +=1
 
 imgs = np.array(imgs)
 
@@ -81,25 +86,25 @@ original_idx_to_synset = {}
 synset_to_name = {}
 
 for i in range(1000):
-    ilsvrc2012_id = int(meta["synsets"][i,0][0][0][0])
-    synset = meta["synsets"][i,0][1][0]
-    name = meta["synsets"][i,0][2][0]
-    original_idx_to_synset[ilsvrc2012_id] = synset
-    synset_to_name[synset] = name
+	ilsvrc2012_id = int(meta["synsets"][i,0][0][0][0])
+	synset = meta["synsets"][i,0][1][0]
+	name = meta["synsets"][i,0][2][0]
+	original_idx_to_synset[ilsvrc2012_id] = synset
+	synset_to_name[synset] = name
 
 synset_to_keras_idx = {}
 keras_idx_to_name = {}
 f = open("synset_words.txt","r")
 idx = 0
 for line in f:
-    parts = line.split(" ")
-    synset_to_keras_idx[parts[0]] = idx
-    keras_idx_to_name[idx] = " ".join(parts[1:])
-    idx += 1
+	parts = line.split(" ")
+	synset_to_keras_idx[parts[0]] = idx
+	keras_idx_to_name[idx] = " ".join(parts[1:])
+	idx += 1
 f.close()
 
 def convert_original_idx_to_keras_idx(idx):
-    return synset_to_keras_idx[original_idx_to_synset[idx]]
+	return synset_to_keras_idx[original_idx_to_synset[idx]]
 
 f = open(label_text,"r")
 labels = f.read().strip().split("\n")
@@ -111,13 +116,13 @@ f.close()
 labels = labels.argmax(axis=1)
 
 def top_k_accuracy(y_true, y_pred, k=1):
-    argsorted_y = np.argsort(y_pred)[:,-k:]
-    return np.any(argsorted_y.T == y_true, axis=0).mean()
+	argsorted_y = np.argsort(y_pred)[:,-k:]
+	return np.any(argsorted_y.T == y_true, axis=0).mean()
 
-# global_pred = model.predict(imgs, verbose = 1)
-# global_acc1 = top_k_accuracy(labels, global_pred,1)
+global_pred = model.predict(imgs[:testSize], verbose = 1)
+global_acc1 = top_k_accuracy(labels[:testSize], global_pred,1)
 # global_acc5 = top_k_accuracy(labels, global_pred,5)
-# print(global_acc1)
+print(global_acc1)
 # print(global_acc5)
 
 
@@ -127,24 +132,23 @@ def top_k_accuracy(y_true, y_pred, k=1):
 #print(type(model))
 
 def testingFunction(X_test, y_test):
-    global_pred = model.predict(X_test, verbose = 1)
-    global_acc1 = top_k_accuracy(y_test, global_pred,1)
-    print(global_acc1)
-    return global_acc1
+	global_pred = model.predict(X_test, verbose = 1)
+	global_acc1 = top_k_accuracy(y_test, global_pred,1)
+	print(global_acc1)
+	return global_acc1
 
 model= keras.models.load_model('model.h5')
-secureWeights = model.get_weights()
-
-#model.summary()
-
-milr = MILR(model)
 model.summary()
 
+secureWeights = model.get_weights()
+
+milr = MILR(model)
+
 # def RBERefftec(self,rounds, error_Rate, testFunc, TestingData, testNumber)
-milr.RBERefftec(40, [1E-1,1.5E-1,1E-2,1.5E-2,1E-3,1.5E-3,1E-4,1.5E-4,1E-5,1.5E-5,1E-6,1.5E-6,1E-7,1.5E-7], testingFunction,(imgs, labels), 1)
+milr.RBERefftec(40, [1E-1,1.5E-1,1E-2,1.5E-2,1E-3,1.5E-3,1E-4,1.5E-4,1E-5,1.5E-5,1E-6,1.5E-6,1E-7,1.5E-7], testingFunction,(imgs[:testSize], labels[:testSize]), 1)
 # milr.RBERefftec(2, [1E-5], testingFunction,(X_test, y_test), 1)
 
 model.set_weights(secureWeights)
 # def continousRecoveryTest(self,rounds, error_Rate, testFunc, TestingData, testNumber)
-milr.continousRecoveryTest(40, [1E-1,1.5E-1,1E-2,1.5E-2,1E-3,1.5E-3,1E-4,1.5E-4,1E-5,1.5E-5,1E-6,1.5E-6,1E-7,1.5E-7], testingFunction, (imgs, labels), 1)
+#milr.continousRecoveryTest(40, [1E-1,1.5E-1,1E-2,1.5E-2,1E-3,1.5E-3,1E-4,1.5E-4,1E-5,1.5E-5,1E-6,1.5E-6,1E-7,1.5E-7], testingFunction, (imgs[:testSize], labels[:testSize]), 1)
 # model.set_weights(secureWeights)
